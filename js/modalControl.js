b47b07d3a40a1138463082c2f1853af063ed9f90
Henrique Chaves <canaldomonza@gmail.com>
2023-01-30 21:08:55 -0300

    feat: add modal option and modify cv file

---
+++ js/modalControl.js
@@ -0,0 +1,11 @@
+const modal = document.querySelector("dialog");
+const btnCloseModal = document.querySelector("dialog > input");
+
+const btnOpenModal = document.querySelector("#btn-open-modal");
+
+btnOpenModal.addEventListener("click", () => {
+  modal.showModal();
+});
+btnCloseModal.addEventListener("click", () => {
+  modal.close();
+});
